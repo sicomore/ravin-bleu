9594e4c556d9853c9954d0e0c4233cb047259896
sicomore <nicolasmoreau.meb@gmail.com>
2019-01-19 19:43:40 +0100

    modif gitignore

--- calendriers/api-id.php
+++
@@ -1,6 +0,0 @@
-<?php
-// define ('API_ID', 'Salt For The GG ApiKey 2019');
-//
-// $hash = openssl_encrypt($apiId, 'AES-128-ECB', API_ID);
-
-return $apiId = 'AIzaSyC6dxVdtzfY-GxHApIMpirNkNiKF2m09Xo';
